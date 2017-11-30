5b5da5853821b2bc4f1ab31aa2bd70dafcbbe586
Affy Yuni Aliyah <32734970+MerahBiru@users.noreply.github.com>
2017-11-30 21:49:55 +0700

    Add files via upload

---
+++ MemasakNasi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("1. Siapkan Beras\n");
+	printf("2. Cuci Beras\n");
+	printf("3. Masukan ke wadah beras\n");
+	printf("4. Masukan ke penanak nasi\n");
+	printf("5. Nyalakan penanak nasi\n");
+	printf("6. Tunggu sampai matang\n");
+return 0;
+}
